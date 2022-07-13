43c51bd0c8de26d39ffa1342ab0b607377fc46ec
roncasabal <roncasabal9@gmail.com>
2022-07-13 18:54:15 +0800

    Update

--- casabal-singson/connect.php
+++ casabal-singson/connect.php
@@ -4,7 +4,20 @@
     $con = new mysqli('localhost', 'root', '', 'test');
 
     if(!$con){
-        die(mysqli_error($con))
+        if(isset($_POST['register']))
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+    
+        $sql="insert into `usersdata`(name, email, password)
+        value('$name','$email','$password')";
+        $result=mysqli_query($con,$sql);
+    
+        if($result){
+            echo"Registered successfully!"
+        }else{
+            die(mysqli_error($con));
+        }
     }
 
     ?>

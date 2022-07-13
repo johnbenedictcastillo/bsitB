53de5fc8e131655c76c7ccc7db8b4dc1bf29add7
symonchristopher-bolinas <101610568+symonchristopher-bolinas@users.noreply.github.com>
2022-07-13 16:49:42 -0700

    Update script.js

--- bolinas-leynes/scripts/script.js
+++ bolinas-leynes/scripts/script.js
@@ -15,12 +15,12 @@ async function saveUser(email, username, password){
 })
   if(data) {
     console.log(data)
-    saveUser(email, username, password);
+    //getStudent();
 
   }
   if(error) {
     console.log(error)
-    return null
+    //return null
 
   }
 

6bbb9113d42ed443a183ea90114f41025fda2e18
roncasabal <roncasabal9@gmail.com>
2022-07-13 19:03:45 +0800

    update

--- casabal-singson/connect.php
+++ casabal-singson/connect.php
@@ -1,23 +1,21 @@
 <?php
 
-    //database connection
-    $con = new mysqli('localhost', 'root', '', 'test');
-
-    if(!$con){
-        if(isset($_POST['register']))
         $name = $_POST['name'];
         $email = $_POST['email'];
         $password = $_POST['password'];
-    
-        $sql="insert into `usersdata`(name, email, password)
-        value('$name','$email','$password')";
-        $result=mysqli_query($con,$sql);
-    
-        if($result){
-            echo"Registered successfully!"
-        }else{
-            die(mysqli_error($con));
-        }
+    //database connection
+    $conn = new mysqli('localhost', 'root', '', 'test');
+
+    if($conn->connect_error){
+        die('Connection failed : '.$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into userdata(name, email, password)
+            values(?,?,?)");
+        $stmt->bind_param("sss"$name, $email, $password);
+        $stmt->execute();
+        echo "Registration Successful";
+        $stmt->close();
+        $conn->close();
     }
 
     ?>

8dc47e09bb350672055b9440c1b7beb665a1b6ab
symonchristopher-bolinas <101610568+symonchristopher-bolinas@users.noreply.github.com>
2022-07-13 16:02:47 -0700

    Update script.js

--- bolinas-leynes/scripts/script.js
+++ bolinas-leynes/scripts/script.js
@@ -6,11 +6,11 @@ const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsIn
 const connection = supabase.createClient(PROJECT_URL,API_KEY)
 
 
-async function saveUser(email, user, password){
+async function saveUser(email, username, password){
 
   const { data, error } = await connection.from("user").insert({
     Email: email,
-    Username: user,
+    Username: username,
     Password: password,
 })
   if(data) {
@@ -31,8 +31,8 @@ $(document).ready(function(){
     $( "#registration" ).submit(function( event ) {
       event.preventDefault();
       let email = $('#emailadd').val();
-      let user = $('#uname').val();
+      let username = $('#uname').val();
       let password = $('#psw').val();
-      saveUser(email,user,password)
+      saveUser(email,username,password)
     });
   });

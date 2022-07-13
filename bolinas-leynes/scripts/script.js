90645b0ac44373b946cac6b7569dd0b3df641fa9
symonchristopher-bolinas <101610568+symonchristopher-bolinas@users.noreply.github.com>
2022-07-13 08:59:43 -0700

    Update script.js

--- bolinas-leynes/scripts/script.js
+++ bolinas-leynes/scripts/script.js
@@ -3,7 +3,7 @@ const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsIn
 
 
 // Create a single supabase client for interacting with your database 
-const connection = supabase.createClient(PROJECT_URL,API_KEY)
+const connection = user.createClient(PROJECT_URL,API_KEY)
 
 
 async function saveUser(email, uname, psw){

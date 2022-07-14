840e6292cd80b6156b636186363cb07370803369
symonchristopher-bolinas <101610568+symonchristopher-bolinas@users.noreply.github.com>
2022-07-14 02:33:45 -0700

    Update script.js

--- bolinas-leynes/scripts/script.js
+++ bolinas-leynes/scripts/script.js
@@ -2,7 +2,6 @@ const PROJECT_URL = "https://unrtwlhsybkttfnqvtgr.supabase.co"
 const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVucnR3bGhzeWJrdHRmbnF2dGdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTc2ODc1NzksImV4cCI6MTk3MzI2MzU3OX0.08okn3PJ6NcEyCk3OEQjrZRrMvKWMjV_YIQ6KP__YFw"
 
 
-
 const database = supabase.createClient(PROJECT_URL,API_KEY);
 
 async function saveUser(email, username, password){

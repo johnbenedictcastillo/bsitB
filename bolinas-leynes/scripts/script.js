43f2dce14f9e06adf1c440ef98036762e9672685
symonchristopher-bolinas <101610568+symonchristopher-bolinas@users.noreply.github.com>
2022-07-13 17:23:46 -0700

    Update script.js

--- bolinas-leynes/scripts/script.js
+++ bolinas-leynes/scripts/script.js
@@ -1,38 +1,38 @@
 const PROJECT_URL = "https://unrtwlhsybkttfnqvtgr.supabase.co"
 const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVucnR3bGhzeWJrdHRmbnF2dGdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTc2ODc1NzksImV4cCI6MTk3MzI2MzU3OX0.08okn3PJ6NcEyCk3OEQjrZRrMvKWMjV_YIQ6KP__YFw"
 
-
-// Create a single supabase client for interacting with your database 
 const connection = supabase.createClient(PROJECT_URL,API_KEY)
 
+const signUpUser = async (email, username, password) => {
+  const { user, session, error } = await connection.auth.signUp(
+    {
+      email,
+	    username,
+      password
+    }
+  )
 
-async function saveUser(email, username, password){
+  return user;
+}
 
-  const { data, error } = await connection.from("user").insert({
-    email: email,
-    username: username,
-    password: password,
-})
-  if(data) {
-    console.log(data)
-    //getStudent();
-
-  }
-  if(error) {
-    console.log(error)
-    //return null
-
-  }
+const logInUser = async (username, password) => {
+  const { user, session, error } = await connection.auth.signIn(
+    {
+      username,
+      password
+    }
+  )
+  console.log("user: ", user)
+  console.log("session: ", connection.auth.session())
 
 }
 
-$(document).ready(function(){
-  
-    $( "#registration" ).submit(function( event ) {
-      event.preventDefault();
-      let email = $('#emailadd').val();
-      let username = $('#uname').val();
-      let password = $('#psw').val();
-      saveUser(email,username,password);
-    });
-  });
+      $("#registration").on("submit", async function (e) {
+      e.preventDefault();
+			let email = $("#emailadd").val();
+      let username = $("#uname").val();
+      let password = $("#psw").val();
+
+      showPreloader();
+      let user = await signUpUser(email, username, password);
+ })

a5cec7437610e9405e489f4120185a2cc19b0b59
symonchristopher-bolinas <101610568+symonchristopher-bolinas@users.noreply.github.com>
2022-07-14 00:48:36 -0700

    Update script.js

--- bolinas-leynes/scripts/script.js
+++ bolinas-leynes/scripts/script.js
@@ -1,6 +1,7 @@
 const PROJECT_URL = "https://unrtwlhsybkttfnqvtgr.supabase.co"
 const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVucnR3bGhzeWJrdHRmbnF2dGdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTc2ODc1NzksImV4cCI6MTk3MzI2MzU3OX0.08okn3PJ6NcEyCk3OEQjrZRrMvKWMjV_YIQ6KP__YFw"
 
+
 const connection = supabase.createClient(PROJECT_URL,API_KEY)
 
 const usersaccount=document.querySelector("#registration");
